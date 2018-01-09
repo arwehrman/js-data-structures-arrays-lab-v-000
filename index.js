a827639fa2f9db74c22ea3b25b2359380d8c9b9f
Amanda Wehrman <arwehrman@gmail.com>
2018-01-09 01:46:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function destructivelyRemoveFirstDriver(){
 }
 
 function appendDriver(name) {
-  drivers.splice(name);
+  return [...drivers, name];
 }
 
 function prependDriver(name) {
-
+  return [name, ...drivers];
 }
